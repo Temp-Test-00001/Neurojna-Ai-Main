c87ac057401772c867c10c02c352ec6ccbe453d1
Temp-Test-00001 <temper0arymail@gmail.com>
2026-07-05 00:51:13 +0530

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcw1ivK0D4ild86VA1zf5g3owPzcZ8FEk",
-  authDomain: "neurojnaai.firebaseapp.com",
-  projectId: "neurojnaai",
-  storageBucket: "neurojnaai.firebasestorage.app",
-  messagingSenderId: "310537464756",
-  appId: "1:310537464756:web:14623130ad963daf2ae0e1",
-  measurementId: "G-3EZ217DLFY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
